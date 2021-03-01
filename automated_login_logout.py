adecb3b60924b71cf5f62613f94c7438116e047e
Defi Sartika H <47235218+defikah@users.noreply.github.com>
2021-03-01 10:46:58 +0700

    auto login - logout

---
+++ automated_login_logout.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+
+driver.get("https://github.com/login")
+time.sleep(2)
+driver.find_element_by_id("login_field").send_keys("sartikahsb15@gmail.com")
+time.sleep(1)
+driver.find_element_by_id("password").send_keys("3Perawang")
+time.sleep(2)
+driver.find_element_by_xpath("/html/body/div[3]/main/div/div[4]/form/input[14]").click()
+time.sleep(5)
+driver.find_element_by_xpath("/html/body/div[1]/header/div[7]/details/summary/span[2]").click()
+time.sleep(3)
+driver.find_element_by_xpath("/html/body/div[1]/header/div[7]/details/details-menu/form/button").click()
+time.sleep(5)
+driver.close()
+print ("success running robot program")
