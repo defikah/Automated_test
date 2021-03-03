6914d07a49e0adabd5dff54830bff0248d992ad9
Defi Sartika H <47235218+defikah@users.noreply.github.com>
2021-03-03 18:19:34 +0700

    automated test gistgithub

---
+++ gistgithubautomated.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+
+driver.get("https://gist.github.com/discover")
+time.sleep(1)
+driver.find_element_by_xpath("/html/body/div[1]/div/div[5]/a[1]").click()
+time.sleep(1)
+driver.find_element_by_id("login_field").send_keys("sartikahsb15@gmail.com")
+time.sleep(1)
+driver.find_element_by_id("password").send_keys("3Perawang")
+time.sleep(1)
+driver.find_element_by_xpath("/html/body/div[3]/main/div/div[3]/form/input[14]").click()
+time.sleep(1)
+driver.find_element_by_xpath("/html/body/div[1]/div/div[6]/details/summary").click()
+time.sleep(3)
+driver.find_element_by_xpath("/html/body/div[1]/div/div[6]/details/details-menu/form/button").click()
+time.sleep(3)
+driver.find_element_by_xpath("/html/body/div[4]/main/div/form/input[2]").click()
+time.sleep(2)
+driver.close()
+print ("success running robot program")
